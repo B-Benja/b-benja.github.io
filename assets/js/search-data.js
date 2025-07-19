e80dc6400ef78ebeeb04493682341c2cca28c5b0
B-Benja <53045556+B-Benja@users.noreply.github.com>
2025-07-19 09:31:40 +0000

    Deploying to gh-pages from @ B-Benja/b-benja.github.io@580a2594ea029a8ce8b32247e02ff8ad044fa668 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,31 +37,31 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "projects-tracing-science-in-patents",
-          title: 'Tracing Science in Patents',
-          description: "identifying patent–paper pairs through visual and textual overlap",
+        },{id: "projects-patent-paper-pairs",
+          title: 'Patent-Paper-Pairs',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-scraping-patent-claims",
-          title: 'Scraping Patent Claims',
-          description: "extracting legal boundaries of inventions",
+            },},{id: "projects-webscraping",
+          title: 'Webscraping',
+          description: "Scraping Patent Claims",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-zotero",
           title: 'Zotero',
           description: "Tutorial for the open-source bibliography and citation manager Zotero.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_other/";
+              window.location.href = "/projects/3_project/";
             },},{id: "projects-chocolatey",
           title: 'Chocolatey',
           description: "how to use the package manager for Windows",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_other/";
+              window.location.href = "/projects/4_project/";
             },},{id: "projects-scraping-patent-claims",
           title: 'Scraping Patent Claims',
           description: "extracting legal boundaries of inventions",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_other/";
+              window.location.href = "/projects/5_project/";
             },},{id: "teaching-economics-of-innovation-introduction",
           title: 'Economics of Innovation: Introduction',
           description: "",
